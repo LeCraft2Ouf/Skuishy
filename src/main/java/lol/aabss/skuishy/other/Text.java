9840294e18f9cb74050fb854b6b140aff515280d
Fusezion <fusezionstream@gmail.com>
2024-12-12 23:48:57 -0500

    TinyText - fix color code support

--- src/main/java/lol/aabss/skuishy/other/Text.java
+++ src/main/java/lol/aabss/skuishy/other/Text.java
@@ -26,7 +26,7 @@ public class Text {
             if (index != -1){
                 string.append(superTiny ? supertiny.get(index) : tiny.get(index));
             } else {
-                if (st.equals("&")){
+                if (st.equals("&") || st.equals("§")){
                     b = true;
                 }
                 string.append(st);
