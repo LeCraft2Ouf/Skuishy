4cd31b7cded2d6155d960e7f63e4a4237c2f2790
Fusezion <fusezionstream@gmail.com>
2025-01-25 13:34:36 -0500

    SkinWrapper.java - Comment out file

--- src/main/java/lol/aabss/skuishy/other/SkinWrapper.java
+++ src/main/java/lol/aabss/skuishy/other/SkinWrapper.java
@@ -1,36 +1,8 @@
 package lol.aabss.skuishy.other;
 
-import ch.njol.skript.Skript;
-import com.destroystokyo.paper.profile.PlayerProfile;
-import com.destroystokyo.paper.profile.ProfileProperty;
-import lol.aabss.skuishy.Skuishy;
-import net.md_5.bungee.api.ChatColor;
-import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
-import org.bukkit.entity.Player;
-import org.bukkit.profile.PlayerTextures;
-import org.mineskin.GenerateOptions;
-import org.mineskin.Java11RequestHandler;
-import org.mineskin.MineSkinClient;
-import org.mineskin.data.SkinInfo;
-import org.mineskin.data.TextureInfo;
-import org.mineskin.data.ValueAndSignature;
-import org.mineskin.data.Variant;
-import org.mineskin.data.Visibility;
-import org.mineskin.request.GenerateRequest;
-
-import javax.annotation.Nullable;
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-
 public class SkinWrapper {
 
+/*
     public static MineSkinClient client = MineSkinClient.builder().userAgent("Skuishy-Agent").requestHandler(Java11RequestHandler::new).build();
     public static GenerateOptions options = GenerateOptions.create().name("Skuishy-Upload").variant(Variant.AUTO).visibility(Visibility.UNLISTED);
 
@@ -132,5 +104,6 @@ public class SkinWrapper {
             return CompletableFuture.failedFuture(exception);
         }
     }
+*/
 
 }
